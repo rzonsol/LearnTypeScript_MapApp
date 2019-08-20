d0256845788a468f0defb5ba95a6f2f7695e9ab8
Piotr Rzonsowski <rzonsol@gmail.com>
2019-08-20 13:24:51 +0200

    Show markers on map

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { User } from './User';
 import { GoogleMap } from './GoogleMap';
-// import { Company } from './Company';
+import { Company } from './Company';
 
 const user = new User();
+const company = new Company();
 
 const map = new GoogleMap('map');
+
+const userMarker = map.addMarker(user);
+const companyMarker = map.addMarker(company);
